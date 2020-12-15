04d320a9e5a39b5c08337fdb13ae7e60c8a618a8
khamis-hesham <khamis1997.kh@gmail.com>
2020-12-15 18:30:46 +0200

    add query database function Relates #5

---
+++ src/database/queries/deleteTodo.js
@@ -0,0 +1,10 @@
+const connection = require("../config/connection");
+
+const deleteTodo = (user_id, todo_id) => {
+  return connection.query({
+    text: "DELETE FROM todo WHERE todo_id = $1 and user_id = $2",
+    values: [user_id, todo_id],
+  });
+};
+
+module.exports = deleteTodo;
---
+++ src/database/queries/getTodos.js
@@ -0,0 +1,10 @@
+const connection = require("../config/connection");
+
+const getTodos = (id) => {
+  return connection.query({
+    text: "SELECT text_content , created_at FROM todo WHERE user_id = $1",
+    values: [id],
+  });
+};
+
+module.exports = getTodos;
